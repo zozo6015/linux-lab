6c859d5af1800d1ef2bc7fcb39915bd30dfef226
root <root@debian.z0z0.tk>
2019-03-17 20:36:54 -0400

    fixing create partition script

--- config/includes.chroot/opt/01-create-partition.sh
+++ config/includes.chroot/opt/01-create-partition.sh
@@ -5,9 +5,9 @@ DISK_EXIST=$(echo $?)
 
 if [ ${DISK_EXIST} -eq 0 ]; then
   fdisk -l | grep sdb3
-  OUTPUT=$(grep $?)
-  if [ ${OUTPUT} -qa "1" ]; then
+  OUTPUT=$(echo $?)
+  if [ ${OUTPUT} -eq 1 ]; then
     echo 'type=b' | sfdisk -a /dev/sdb --force
-    reboot
+    /sbin/reboot
   fi
 fi
